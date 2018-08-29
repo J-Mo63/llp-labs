3c98982da74d11a8bedcd031beaf7b0ce9c23cb0
Jonathan Moallem <jdmoallem@gmail.com>
2018-08-29 11:06:43 -0400

    Completed lab1

---
+++ lab1_jmoallem_206/lab1_jmoallem_206.c
@@ -0,0 +1,15 @@
+/*
+ * Jonathan Moallem G01169880
+ * CS 262, Lab Section 206
+ * Lab 1
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    printf("Hello world!\n");
+    printf("My name is Jonathan Moallem.\n");
+    return 0;
+}
